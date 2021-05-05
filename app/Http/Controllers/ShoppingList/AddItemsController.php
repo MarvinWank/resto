581987bf2cf5246d15c8af4963ab775454f2b7a8
Marvin Wank <marvin.wank@outlook.de>
2021-05-05 15:11:05 +0200

    Adds API-Endpoint

--- app/Http/Controllers/ShoppingList/AddItemsController.php
+++ app/Http/Controllers/ShoppingList/AddItemsController.php
@@ -4,18 +4,26 @@
 namespace App\Http\Controllers\ShoppingList;
 
 
+use App\Factories\ShoppingListFactory;
+use App\Factories\UserFactory;
 use App\Http\Controllers\Controller;
-use App\Models\State;
 use App\Value\IngredientsSet;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AddItemsController extends Controller
 {
-    public function addItems(Request $request, State $state): JsonResponse
+    public function addItems(Request $request, UserFactory $userFactory, ShoppingListFactory $shoppingListFactory): JsonResponse
     {
         $ingredients = \Safe\json_decode($request->post('ingredients'), true);
         $ingredients = IngredientsSet::fromArray($ingredients);
+        $user = $userFactory->currentUser();
 
+        $newList = $shoppingListFactory->addItemsToShoppingList($user, $ingredients);
+
+        return response()->json([
+            "status" => "ok",
+            "shoppingList" => $newList->toArray()
+        ]);
     }
 }
