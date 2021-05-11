5157f828d21ed941193c67aa0e8628fad6756ad5
Marvin Wank <marvin.wank@outlook.de>
2021-05-11 18:30:27 +0200

    Vervollständige deploy-script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 git pull
-git checkout $1
+git checkout prod
 
+php74 bin/composer.phar install
+php74 bin/composer.phar run phpstan
+php74 bin/composer.phar run phpunit
+
+cd frontend
+yarn
+yarn build
