eab6e5588d13eacebd5d431d2e494e1223242844
Marvin Wank <marvin.wank@outlook.de>
2021-05-12 16:13:28 +0200

    Adds password and email congruency check

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,9 @@
-php7.4 artisan down
-
 git pull
 git checkout prod
 
 cp .env.prod .env
 
 php7.4 bin/composer.phar install --optimize-autoloader --no-dev
-php7.4 bin/composer.phar run phpstan
-php7.4 bin/composer.phar run phpunit
 
 php7.4 artisan migrate
 
@@ -16,4 +12,3 @@ yarn
 yarn build
 
 
-php7.4 artisan up
--- frontend/src/views/register/Register.vue
+++ frontend/src/views/register/Register.vue
@@ -10,20 +10,36 @@
                                                                  class="link-primary">anmeldem</a>
         </h2>
 
+        <div class="form-group">
+            <label for="username">Name</label>
+            <input v-model="username" type="text" class="form-control" id="username"
+                   placeholder="Name">
+        </div>
+
         <div class="row">
             <div class="col-12 col-lg-6">
                 <div class="form-group">
                     <label for="email_1">E-Mail Adresse</label>
-                    <input v-model="email_1" type="email" class="form-control" id="email_1"
-                           placeholder="E-Mail Adresse">
+                    <input v-model="email_1"
+                           @blur="checkEmail"
+                           type="email"
+                           class="form-control"
+                           id="email_1"
+                           placeholder="E-Mail Adresse"
+                    >
                 </div>
             </div>
 
             <div class="col-12 col-lg-6">
                 <div class="form-group">
                     <label for="email_2">E-Mail Adresse wiederholen</label>
-                    <input v-model="email_2" type="email" class="form-control" id="email_2"
-                           placeholder="E-Mail Adresse wiederholen">
+                    <input v-model="email_2"
+                           @blur="checkEmail"
+                           type="email"
+                           class="form-control"
+                           id="email_2"
+                           placeholder="E-Mail Adresse wiederholen"
+                    >
                 </div>
             </div>
 
@@ -34,7 +50,7 @@
             <div class="col-12 col-lg-6">
                 <div class="form-group">
                     <label for="password_1">Passwort</label>
-                    <input v-model="password_1" type="password" class="form-control" id="password_1"
+                    <input v-model="password_1" @blur="checkPassword" type="password" class="form-control" id="password_1"
                            placeholder="Passwort">
                 </div>
             </div>
@@ -42,14 +58,14 @@
             <div class="col-12 col-lg-6">
                 <div class="form-group">
                     <label for="password_2">Passwort wiederholen</label>
-                    <input v-model="password_2" type="password" class="form-control" id="password_2"
+                    <input v-model="password_2" @blur="checkPassword" type="password" class="form-control" id="password_2"
                            placeholder="Passwort wiederholen">
                 </div>
             </div>
         </div>
 
 
-        <div v-if="registerError" class="alert alert-danger">Login Fehlgeschlagen!</div>
+        <div v-if="registerError" class="alert alert-danger">{{registerErrorMessage}}</div>
         <button type="submit" :class="getLoginButtonClasses" @click="attemptRegister">Registrieren</button>
 
     </div>
@@ -64,15 +80,49 @@ import router from "@/router/router";
 @Component
 export default class Register extends Vue {
 
+    username = '';
     email_1 = '';
     email_2 = '';
     password_1 = '';
     password_2 = '';
-    registerError: false;
+    registerError = false;
+    registerErrorMessage: '';
 
 
     login() {
         router.push({name: "Login"})
+    }
+
+    checkEmail(){
+        if (this.email_1 === '' || this.email_2 === '' ){
+            return;
+        }
+
+        if (this.email_1 === this.email_2){
+            this.registerError = false;
+            this.registerErrorMessage = '';
+        }
+
+        if (this.email_1 !== this.email_2){
+            this.registerError = true;
+            this.registerErrorMessage = "E-Mail Adressen stimmen nicht überein"
+        }
+    }
+
+    checkPassword(){
+        if (this.password_1 === '' || this.password_2 === '' ){
+            return;
+        }
+
+        if (this.password_1 === this.password_2){
+            this.registerError = false;
+            this.registerErrorMessage = '';
+        }
+
+        if (this.password_1 !== this.password_2){
+            this.registerError = true;
+            this.registerErrorMessage = "Passwörter stimmen nicht überein"
+        }
     }
 
     attemptRegister() {
@@ -84,7 +134,7 @@ export default class Register extends Vue {
             "btn": true,
             "btn-primary": true,
             "btn-block": true,
-            "disabled": this.email_1 === '' || this.email_2 === '' || this.password_1 === '' || this.password_2 === ''
+            "disabled": this.username === '' || this.email_1 === '' || this.email_2 === '' || this.password_1 === '' || this.password_2 === ''
         }
     }
 
