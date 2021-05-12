7fddfcc0ca4feaf570a99b311e6e2848765e4c3f
Marvin Wank <marvin.wank@outlook.de>
2021-05-12 20:16:25 +0200

    Styling

--- frontend/src/views/home/items/RecipeCard.vue
+++ frontend/src/views/home/items/RecipeCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="row recipe-card">
         <div class="col-12 inner">
-            <div class="title cursor-pointer" @click="showRecipe">{{ recipe.title }}</div>
+            <div class="title link-hover cursor-pointer" @click="showRecipe">{{ recipe.title }}</div>
 
             <div class="row justify-content-between pt-2 content">
                 <div class="col-6" @click="showRecipe">
